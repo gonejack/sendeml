f5f90278389fa4cfbe64fd64043d950343f4f5d8
gonejack <igonejack@gmail.com>
2021-05-27 16:38:31 +0800

    update
    Signed-off-by: gonejack <igonejack@gmail.com>

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.16
 
 require (
 	github.com/antonfisher/nested-logrus-formatter v1.3.0
-	github.com/jordan-wright/email v4.0.1-0.20210109023952-943e75fe5223+incompatible
+	github.com/gonejack/email v1.0.6
 	github.com/sirupsen/logrus v1.8.0
 	github.com/spf13/cobra v1.1.3
 )
--- send.go
+++ send.go
@@ -4,11 +4,12 @@ import (
 	"crypto/tls"
 	"errors"
 	"fmt"
+	"io/fs"
 	"net/smtp"
 	"os"
 	"path/filepath"
 
-	"github.com/jordan-wright/email"
+	"github.com/gonejack/email"
 	"github.com/sirupsen/logrus"
 )
 
@@ -23,10 +24,22 @@ type config struct {
 
 type sender struct {
 	config
-	auth smtp.Auth
+	auth   smtp.Auth
+	client *smtp.Client
 }
 
 func (s *sender) sendAndMove(emails []string) {
+	client, err := s.getClient()
+	if err != nil {
+		logrus.WithError(err).Fatalf("get client failed")
+		return
+	}
+	s.client = client
+	defer func() {
+		_ = s.client.Quit()
+		_ = s.client.Close()
+	}()
+
 	for _, eml := range emails {
 		log := logrus.WithField("email", eml)
 
@@ -36,18 +49,17 @@ func (s *sender) sendAndMove(emails []string) {
 			log.WithError(err).Errorf("send failed")
 			continue
 		}
-		log.Info("sent")
+		log.Debugf("sent")
 
 		rename := filepath.Join(sentDir, filepath.Base(eml))
 		index := 1
 		for {
-			_, err := os.Stat(rename)
-			if errors.Is(err, os.ErrNotExist) {
+			if _, err := os.Open(rename); errors.Is(err, fs.ErrNotExist) {
 				break
-			} else {
-				rename = fmt.Sprintf("%s#%d", rename, index)
-				index += 1
 			}
+
+			rename = fmt.Sprintf("%s#%d", rename, index)
+			index += 1
 		}
 		err = os.Rename(eml, rename)
 		if err != nil {
@@ -55,7 +67,6 @@ func (s *sender) sendAndMove(emails []string) {
 		}
 	}
 }
-
 func (s *sender) sendEmail(eml string) (err error) {
 	file, err := os.Open(eml)
 	if err != nil {
@@ -71,16 +82,42 @@ func (s *sender) sendEmail(eml string) (err error) {
 	e.From = s.getFrom()
 	e.To = []string{s.getTo()}
 
+	return e.SendWithClient(s.client)
+}
+func (s *sender) getClient() (c *smtp.Client, err error) {
 	addr := fmt.Sprintf("%s:%d", s.Host, s.Port)
 	auth := s.getAuth()
 
-	if s.Port == 465 {
-		return e.SendWithTLS(addr, auth, &tls.Config{ServerName: s.Host})
-	} else {
-		return e.Send(addr, auth)
+	c, err = smtp.Dial(addr)
+	if err != nil {
+		return
 	}
-}
 
+	err = c.Hello("localhost")
+	if err != nil {
+		return
+	}
+
+	ok, _ := c.Extension("STARTTLS")
+	if ok {
+		err = c.StartTLS(&tls.Config{ServerName: s.Host})
+		if err != nil {
+			return
+		}
+	}
+
+	if auth != nil {
+		ok, _ := c.Extension("AUTH")
+		if ok {
+			err = c.Auth(auth)
+			if err != nil {
+				return
+			}
+		}
+	}
+
+	return
+}
 func (s *sender) getFrom() string {
 	if *argFrom != "" {
 		return *argFrom
